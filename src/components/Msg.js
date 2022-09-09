331cb0b50109744c049883a23aaa3153206ce706
neineiatca <sen.an.ottawa@gmail.com>
2022-09-08 23:27:13 -0400

    msg component aligns with figma

--- src/components/Msg.js
+++ src/components/Msg.js
@@ -3,12 +3,18 @@ export const Msg = ({ txt }) => {
     <div
       style={{
         height: "50px",
-        width: "100px",
-        margin: "auto",
-        border: "solid 1px black",
+        width: "500px",
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop: "50px",
+        marginBottom: "50px",
+        backgroundColor: "#ffecec",
+        textAlign: "left",
+        display: "flex",
+        borderRadius: "66px",
       }}
     >
-      {txt}
+      <span style={{ marginLeft: "17px", marginTop: "10px" }}>{txt}</span>
     </div>
   );
 };
