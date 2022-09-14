7c99f5eb2d475d3d3443961dd3ec041c58b97305
SEN AN <senan@SENs-MacBook-Air.local>
2022-09-13 21:10:58 -0400

    load msgs when init

--- src/components/Msgs.js
+++ src/components/Msgs.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Msg } from "./Msg";
 import { fetchAllApi } from "../api/api";
@@ -7,22 +7,22 @@ import { getContext } from "../common/getProvider";
 export const Msgs = ({ wallet }) => {
   const [msgs, setMsgs] = useState([]);
 
-  async function fetchAllCb() {
-    const { provider, program, baseAccount, SystemProgram } = await getContext({
-      wallet,
-    });
-    let allMsgs = await fetchAllApi({ provider, program, baseAccount });
-    setMsgs(allMsgs);
-  }
+  useEffect(() => {
+    async function fetchAllCb() {
+      const { provider, program, baseAccount, SystemProgram } = await getContext({
+        wallet,
+      });
+      let allMsgs = await fetchAllApi({ provider, program, baseAccount });
+      setMsgs(allMsgs);
+    }
+
+    (async function () {
+      await fetchAllCb()
+    })()
+  }, [])
 
   return (
     <>
-      <button
-        onClick={fetchAllCb}
-        style={{ backgroundColor: "blue", color: "white" }}
-      >
-        fetch
-      </button>
       {msgs.map((m, i) => {
         return <Msg key={i} txt={m.account.field1} />;
       })}
