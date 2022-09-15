cc1057f8b70b7a520806e4b3a27d4a31a8d99371
neineiatca <sen.an.ottawa@gmail.com>
2022-09-15 16:03:02 -0400

    msg ui update

--- src/components/Msg.js
+++ src/components/Msg.js
@@ -1,6 +1,5 @@
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 export const Msg = ({ txt }) => {
   return (
@@ -10,22 +9,46 @@ export const Msg = ({ txt }) => {
         width: "516px",
         border: "solid 1px #e8e4e4",
         backgroundColor: "#ffecec",
-        textAlign: "left",
-        display: "flex",
-        justifyContent: "space-between",
         marginLeft: "auto",
-        marginRight: "auto"
+        marginRight: "auto",
       }}
     >
-      <span style={{ marginLeft: "20px", marginTop: "20px" }}>{txt}</span>
-      <span style={{
-        marginLeft: "auto", marginRight: "20px", marginTop: "auto", marginBottom: "62px",
-        fontFamily: 'Inter',
-        fontStyle: 'normal',
-        fontWeight: 300,
-        fontSize: '16px'
-      }}>{txt}</span>
-      <FontAwesomeIcon icon={faCoffee} />
+      <span style={{ display: "flex", height: "100px" }}>
+        <span style={{ marginLeft: "20px", marginTop: "20px" }}>{txt}</span>
+        <span
+          style={{
+            marginLeft: "auto",
+            marginRight: "20px",
+            marginTop: "auto",
+            marginBottom: "20px",
+            fontFamily: "Inter",
+            fontStyle: "normal",
+            fontWeight: 300,
+            fontSize: "16px",
+          }}
+        >
+          {txt}
+        </span>
+      </span>
+      <span
+        style={{
+          marginLeft: "auto",
+          marginRight: "20px",
+          marginTop: "auto",
+          marginBottom: "20px",
+          height: "29px",
+          display: "flex",
+          justifyContent: "end",
+          color: "red",
+        }}
+      >
+        <div style={{ marginLeft: "auto", marginRight: "10px" }}>
+          <FontAwesomeIcon icon={faCoffee} />
+        </div>
+        <div style={{ marginLeft: "10px", marginRight: "10px" }}>
+          <FontAwesomeIcon icon={faCoffee} />
+        </div>
+      </span>
     </div>
   );
 };
