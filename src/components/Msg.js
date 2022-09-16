fca256f24a619b6016adac539d5904a2e8931148
neineiatca <sen.an.ottawa@gmail.com>
2022-09-16 10:33:19 -0400

    added styled-component

--- src/components/Msg.js
+++ src/components/Msg.js
@@ -3,57 +3,56 @@ import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 import styled from "styled-components";
 
-const MyButton = styled.button``;
+const MsgContainer = styled.div`
+  height: 129px;
+  width: 516px;
+  border: 1px solid #e8e4e4;
+  background-color: #ffecec;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const ContentSection = styled.div`
+  display: flex;
+  height: 100px;
+`;
+
+const ContentText = styled.p`
+  margin-left: 20px;
+  margin-top: 20px;
+`;
+
+const AuthorText = styled.p`
+  margin: auto 20px 20px auto;
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+`;
+
+const ButtonsSection = styled.div`
+  margin: auto 20px 20px auto;
+  height: 29px;
+  display: flex;
+  justify-content: end;
+  color: red;
+`;
 
 export const Msg = ({ txt }) => {
   return (
-    <div
-      style={{
-        height: "129px",
-        width: "516px",
-        border: "solid 1px #e8e4e4",
-        backgroundColor: "#ffecec",
-        marginLeft: "auto",
-        marginRight: "auto",
-      }}
-    >
-      <span style={{ display: "flex", height: "100px" }}>
-        <MyButton>my button</MyButton>
-        <span style={{ marginLeft: "20px", marginTop: "20px" }}>{txt}</span>
-        <span
-          style={{
-            marginLeft: "auto",
-            marginRight: "20px",
-            marginTop: "auto",
-            marginBottom: "20px",
-            fontFamily: "Inter",
-            fontStyle: "normal",
-            fontWeight: 300,
-            fontSize: "16px",
-          }}
-        >
-          {txt}
-        </span>
-      </span>
-      <span
-        style={{
-          marginLeft: "auto",
-          marginRight: "20px",
-          marginTop: "auto",
-          marginBottom: "20px",
-          height: "29px",
-          display: "flex",
-          justifyContent: "end",
-          color: "red",
-        }}
-      >
+    <MsgContainer>
+      <ContentSection>
+        <ContentText>{txt}</ContentText>
+        <AuthorText>{txt}</AuthorText>
+      </ContentSection>
+      <ButtonsSection>
         <div style={{ marginLeft: "auto", marginRight: "10px" }}>
           <FontAwesomeIcon icon={faCoffee} />
         </div>
         <div style={{ marginLeft: "10px", marginRight: "10px" }}>
           <FontAwesomeIcon icon={faCoffee} />
         </div>
-      </span>
-    </div>
+      </ButtonsSection>
+    </MsgContainer>
   );
 };
